dc821365b4e5972e2e76e1af4c9bd558bc79f579
Wayfarer123 <nikitaokhotnikov@mail.ru>
2024-11-20 16:12:49 +0300

    fixed accuracy calculation, add docstrings

--- problib/mdl/Trainer.py
+++ problib/mdl/Trainer.py
@@ -9,6 +9,16 @@ class Trainer:
         model=None,
         train_config=None
     ):
+        """
+        Initialize the Trainer object.
+
+        Parameters
+        ----------
+        model : nn.Module
+            Model to be trained.
+        train_config : dict
+            Configuration for training. Must be provided either here or in .train() method
+        """
         self.model = model
         self.train_config = train_config if train_config is not None else {}
 
@@ -24,6 +34,47 @@ class Trainer:
         lr=None,
         evaluate_every=-1,  # -1 for no evaluation
     ):
+
+        """
+        Train the model using the provided data loaders and training configuration.
+
+        Parameters
+        ----------
+        train_loader : DataLoader, optional
+            DataLoader for training data. Must be provided if not specified in train_config.
+        val_loader : DataLoader, optional
+            DataLoader for validation data. If not provided, evaluation will be skipped.
+        model : nn.Module, optional
+            Model to be trained. If not provided, the model must be set during initialization.
+        n_epochs : int, optional
+            Number of training epochs. If not provided, defaults to the value in train_config or 1.
+        loss_function : str or callable, optional
+            Loss function for training. Defaults to the value in train_config or 'crossentropy' if not specified.
+        optimizer : torch.optim.Optimizer or str, optional
+            Optimizer for training. If not provided, defaults to 'Adam'.
+        variational : bool, optional
+            Whether to use variational training. Defaults to the value in train_config or False if not specified.
+        lr : float, optional
+            Learning rate for the optimizer. Defaults to 1e-3 if not specified.
+        evaluate_every : int, optional
+            Frequency of evaluation during training. Defaults to -1 (no evaluation).
+
+        Returns
+        -------
+        nn.Module
+            The trained model.
+
+        Raises
+        ------
+        ValueError
+            If no model or train_loader is provided.
+            If an unknown optimizer or loss function is specified.
+
+        Warnings
+        --------
+        UserWarning
+            If no learning rate, optimizer, loss_function, n_epochs or val_loader provided.
+        """
         if model is not None:
             self.model = model
         if self.model is None:
@@ -49,16 +100,21 @@ class Trainer:
             opt_name = "Adam"
 
         if opt_name == "Adam":
-            optimizer = torch.optim.Adam(self.model.parameters(), lr=lr)
+            optimizer = torch.optim.Adam(self.model.probing_layer.parameters(), lr=lr)
         elif opt_name == "SGD":
-            optimizer = torch.optim.SGD(self.model.parameters(), lr=lr)
+            optimizer = torch.optim.SGD(self.model.probing_layer.parameters(), lr=lr)
         else:
             raise ValueError(f"Unknown optimizer: {opt_name}")
 
-        if n_epochs is None:
-            n_epochs = self.train_config.get("n_epochs", 1)
-        else:
+        if n_epochs is not None:
             self.train_config["n_epochs"] = n_epochs
+        elif "n_epochs" in self.train_config:
+            n_epochs = self.train_config["n_epochs"]
+        else:
+            n_epochs = 1
+            self.train_config["n_epochs"] = n_epochs
+            warnings.warn("No n_epochs provided. Defaulting to 1", UserWarning)
+            
 
         if loss_function is not None:
             if not isinstance(loss_function, str):
@@ -70,6 +126,7 @@ class Trainer:
         else:
             warnings.warn("No loss function provided. Defaulting to crossentropy", UserWarning,)
             loss_function = "crossentropy"
+            self.train_config["loss_function"] = loss_function
 
         if loss_function == "crossentropy":
             loss_function = nn.CrossEntropyLoss()
@@ -80,15 +137,25 @@ class Trainer:
 
         if train_loader is None:
             train_loader = self.train_config.get("train_loader", None)
+            if train_loader is None:
+                raise ValueError("No train_loader provided")
+            
+        if val_loader is None:
+            val_loader = self.train_config.get("val_loader", None)
+            if val_loader is None:
+                warnings.warn("No val_loader provided", UserWarning)
 
         if variational is not None:
             self.train_config["variational"] = variational
+        else:
+            variational = self.train_config.get("variational", False)
 
         if evaluate_every is not None:
             self.train_config["evaluate_every"] = evaluate_every
         elif "evaluate_every" in self.train_config:
             evaluate_every = self.train_config["evaluate_every"]
         else:
+            warnings.warn("No evaluate_every provided. Defaulting to -1 (no evaluation)", UserWarning)
             evaluate_every = -1
 
         losses = []
@@ -117,6 +184,23 @@ class Trainer:
         loss_function, 
         optimizer
     ):
+        """
+        Train the model for one epoch.
+
+        Parameters
+        ----------
+        train_loader: torch.utils.data.DataLoader
+            The data loader for the training data.
+        loss_function: torch.nn.Module
+            The loss function to use for training.
+        optimizer: torch.optim.Optimizer
+            The optimizer to use for training.
+
+        Returns
+        -------
+        list
+            A list of losses for each batch in the epoch.
+        """
         losses = []
         device = next(self.model.parameters()).device
 
@@ -145,12 +229,44 @@ class Trainer:
         return losses
 
     def _forward(self, batch, device="cuda"):
+        """
+        Performs a forward pass of the model using the input batch.
+
+        Parameters
+        ----------
+        batch : tuple
+            A tuple containing input_ids and attention_mask tensors.
+        device : str, optional
+            The device to run the forward pass on, defaults to "cuda".
+
+        Returns
+        -------
+        torch.Tensor
+            The model predictions for the given inputs.
+        """
         input_ids, attention_mask = batch[0].to(device), batch[1].to(device)
         preds = self.model(input_ids, attention_mask)
 
         return preds
     
     def _calulate_metrics(self, output, batch, device="cuda"):
+        """
+        Calculates the given metrics for the given output and batch.
+
+        Parameters
+        ----------
+        output : torch.Tensor
+            The model predictions for the given inputs.
+        batch : tuple
+            A tuple containing input_ids, attention_mask and labels tensors.
+        device : str, optional
+            The device to run the forward pass on, defaults to "cuda".
+
+        Returns
+        -------
+        dict
+            A dictionary containing the calculated metrics.
+        """
         metrics_to_calulate = self.train_config.get("eval_metrics", [])
         metrics = {}
 
@@ -160,7 +276,7 @@ class Trainer:
                 kl_part = self.model.kl_divergence().detach().cpu().numpy()
                 metrics[m] = ce_part + kl_part
             elif m == "accuracy":
-                metrics[m] = (output.argmax(dim=1) == batch[-1]).mean().detach().cpu().numpy()
+                metrics[m] = (output.argmax(dim=-1) == batch[-1].to(device)).float().mean().detach().cpu().numpy()
             else:
                 warnings.warn(f"Unknown metric: {m}", UserWarning)
         
@@ -171,6 +287,26 @@ class Trainer:
         val_loader=None,
         loss_function=None,
     ):  
+        """
+        Evaluates the model on the given validation data.
+
+        Parameters
+        ----------
+        val_loader : torch.utils.data.DataLoader, optional
+            The validation data loader. If not provided, evaluation is skipped and empty validation metrics are returned.
+        loss_function : callable or str, optional
+            The loss function to use for evaluation. If not provided, the loss function specified in the training configuration is used.
+
+        Returns
+        -------
+        dict
+            A dictionary containing the mean validation metrics.
+        
+        Warnings
+        --------
+        UserWarning
+            If no validation data loader is provided and an empty dictionary is returned.
+        """
         if val_loader is not None:
             with torch.no_grad():
                 self.model.eval()
@@ -184,6 +320,8 @@ class Trainer:
                     
                     batch_metrics = self._calulate_metrics(output, batch, device)
 
+                    if loss_function is None:
+                        loss_function = self.train_config.get("loss_function", None)
                     if not self.train_config.get("variational", False) and loss_function is not None:
                         if isinstance(loss_function, str):
                             if loss_function == "crossentropy":
--- problib/mdl/utils.py
+++ problib/mdl/utils.py
@@ -12,6 +12,14 @@ from nltk.corpus import conll2000
 
 class POSTaggingTokenizer:
     def __init__(self):
+        """
+        Constructor for POSTaggingTokenizer class that converts a list of words and their part-of-speech tags into a list of word-ids and tag-ids
+
+        Notes
+        -----
+            Initializes the tag2idx and idx2tag dictionaries and sets the IDs for
+            the special tags <PAD>, <BOS>, <EOS>, and <UNK>.
+        """
         self.idx2tag = {0: '<PAD>', 1: '<BOS>', 2: '<EOS>', 3: '<UNK>'}
         self.pad_token_id = 0
         self.bos_token_id = 1
@@ -22,6 +30,19 @@ class POSTaggingTokenizer:
         return len(self.idx2tag)
     
     def fit(self, sentences_conll):
+        """
+        Learns the tag vocabulary from a list of sentences in the CoNLL format
+
+        Parameters
+        ----------
+        sentences_conll : List[List(tuple)] 
+            A list of sentences where each sentence is a list of tuples (word, tag)
+
+        Returns
+        -------
+        POSTaggingTokenizer (self)
+            The same tokenizer with the learned tag vocabulary
+        """
         idx = len(self.idx2tag)
         for s in sentences_conll:
             for _, word_tag in s:
@@ -32,6 +53,21 @@ class POSTaggingTokenizer:
         return self
 
     def encode(self, sent_words_with_tags):
+        """
+        Converts a list of sentences in the CoNLL format to a list of sentences where
+        each word is replaced with its tag ID. 
+
+        Parameters
+        ----------
+        sent_words_with_tags : List[List(tuple)] 
+            A list of sentences represented as a list of tuples (word, tag) (CoNLL format)
+
+        Returns:
+        List[List(tuple)] | List(tuple)
+            A list of sentences where each word is replaced with its tag ID. If the input
+            contains a single sentence, the output is a single sentence, otherwise it
+            is a list of sentences.
+        """
         if isinstance(sent_words_with_tags[0], tuple):
             sent_words_with_tags = [sent_words_with_tags]        
         sents_w_with_ids = []
@@ -50,9 +86,24 @@ class POSTaggingTokenizer:
         return sents_w_with_ids
 
     def __call__(self, NERs):
+        """
+            see .encode(...)
+        """
         return self.encode(NERs)
 
     def decode(self, ids):
+        """
+        Converts a list of tag IDs to a list of part-of-speech tags
+
+        Parameters
+        ----------
+        ids (list of int): A list of tag IDs
+
+        Returns
+        -------
+        list
+            A list of POS tags corresponding to the tag IDs
+        """
         tags = []
         for idx in ids:
             if self.idx2tag.get(idx) is not None:
@@ -68,11 +119,46 @@ class MDLDataset_POSTagging(Dataset):
         data_path,
         tagging_tokenizer=None
     ):
+        """
+        Constructor for MDLDataset_POSTagging class
+
+        Parameters
+        ----------
+        data_path : str
+            either "conll2000_train.txt" or "conll2000_test.txt" otherwise raises NotImplementedError
+        tagging_tokenizer : POSTaggingTokenizer, optional
+            Part-of-speech tagging tokenizer to use for encoding the data. If None (default), a new tokenizer will be created and fit to the data.
+
+        Returns
+        -------
+        MDLDataset_POSTagging
+            A dataset containing the data in the given file, where each item is a list of words and part-of-speech tags
+        """
         super().__init__()
         self.tagging_tokenizer = tagging_tokenizer
         self.data = self._read_data(data_path)
 
     def _read_data(self, data_path):
+                
+        """
+        Reads data from a specified file path, processes it using the tagging_tokenizer that
+        converts each sentence into a list of words with their corresponding tag IDs.
+
+        Parameters
+        ----------
+        data_path : str
+            The path to the data file. Should contain "conll2000" for the dataset to be processed.
+
+        Returns
+        -------
+        list
+            A list of words with their tag IDs, where each item is a tuple (word, tag_id).
+
+        Raises
+        ------
+        NotImplementedError
+            If the dataset specified in the data_path is not "conll2000".
+        """
         if "conll2000" in data_path:
             nltk.download("conll2000")
             data = conll2000.tagged_sents(data_path.split("_")[-1])
@@ -88,6 +174,20 @@ class MDLDataset_POSTagging(Dataset):
         return len(self.data)
 
     def __getitem__(self, idx):
+        """
+        Retrieves an item from the dataset at the specified index.
+
+        Parameters
+        ----------
+        idx : int
+            Index of the item to retrieve from the dataset.
+
+        Returns
+        -------
+        tuple
+            A tuple where the first element is a list of words and the second
+            element is a list of corresponding POS tag IDs.
+        """
         return [elem[0] for elem in self.data[idx]], [elem[1] for elem in self.data[idx]]
 
 
@@ -102,6 +202,26 @@ class Collator:
         add_special_tokens=True,
         **tokenizer_kwargs
     ):
+        """
+        Initializes the Collator class for processing batches of data before dataloader.
+
+        Parameters
+        ----------
+        tokenizer : transformers.AutoTokenizer
+            Tokenizer used for encoding sentences into token IDs.
+        post_tagging_tokenizer : POSTaggingTokenizer
+            Tokenizer used for encoding part-of-speech tags into tag IDs.
+        max_length : int, optional
+            Maximum length of the tokenized sequences. Default is 512.
+        padding : bool, optional
+            Whether to pad the sequences to the max_length. Default is True.
+        truncation : bool, optional
+            Whether to truncate the sequences to the max_length. Default is True.
+        add_special_tokens : bool, optional
+            Whether to add special tokens to the sequences. Default is True.
+        **tokenizer_kwargs : dict
+            Additional keyword arguments passed to the tokenizer.
+        """
         self.tokenizer = tokenizer
         self.post_tagging_tokenizer = post_tagging_tokenizer
         self.tokenizer_kwargs = tokenizer_kwargs
@@ -116,6 +236,24 @@ class Collator:
 
     def __call__(self, batch):
         
+        """
+        Collates and tokenizes a batch of data, alligns words part-of-speech tags with the corresponding tokens
+
+        Parameters
+        ----------
+        batch : list
+            List of tuples, where each tuple contains a list of words and a list of
+            corresponding part-of-speech tags.
+
+        Returns
+        -------
+        input_ids : torch.Tensor
+            Tensor of token IDs, shape (batch_size, max_length).
+        attention_mask : torch.Tensor
+            Tensor of attention masks, shape (batch_size, max_length).
+        tags_per_token : torch.Tensor
+            Tensor of part-of-speech tags per token, shape (batch_size, max_length).
+        """
         word_lists = [elem[0] for elem in batch]
         tags_per_word = [elem[1] for elem in batch]
 
